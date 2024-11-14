2eeb16004abb253b285124eed7e9b079b5026524
eduardopacompialopez <eduardopacompialopez@gmail.com>
2024-11-13 23:41:01 -0500

    primer commit proyecto virgen

---
+++ src/main/java/com/cibertec/todolistope/TodolistopeApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.todolistope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/todolistope/TodolistopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.todolistope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
